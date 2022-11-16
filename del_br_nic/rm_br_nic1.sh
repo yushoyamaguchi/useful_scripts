601af5733edc0abc3eb5606c4b860868ddea508b
yushoyamaguchi <ysh.824@outlook.jp>
2022-11-16 17:12:36 +0900

    バッククォート

--- del_br_nic/rm_br_nic1.sh
+++ del_br_nic/rm_br_nic1.sh
@@ -2,6 +2,8 @@
 
 #ip link show | grep br- | cut -d " " -f 2 | cut -d ":" -f 1 > nic_list.txt
 
+
+#バッククォート
 for line in `ip link show | grep br- | cut -d " " -f 2 | cut -d ":" -f 1`
 do
   ip link del $line
